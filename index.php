cc76ce26f682539f6cd3917808bf9f6361f4359d
ericjsnover <ericjsnover@yahoo.com>
2016-11-01 13:47:53 -0400

    minor fix

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta name="description" content="Colorful.scss is a CSS-framework for implementing">
   <meta name="author" content="Eric J Snover">
 
-  <link rel="stylesheet" href="colorful.css">
+  <link rel="stylesheet" href="css/colorful.css">
 
   <style>
 	  * {
@@ -253,7 +253,7 @@
 </div>
 
 	<div class='box footer colorful-bg-black'>Copyright <?=date('Y',time())?> Eric J Snover<br>Licensed under the Apache License, Version 2.0 (the "License");<br>you may not use this file except in compliance with the License.<br>You may obtain a copy of the License at<br>http://www.apache.org/licenses/LICENSE-2.0<br>Unless required by applicable law or agreed to in writing, software<br>distributed under the License is distributed on an "AS IS" BASIS,<br>WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.<br>See the License for the specific language governing permissions and<br>limitations under the License.</div>
-
+<br><br><br><br><br>
 <div class='fixed-link colorful-bg-grey-900'>
 <?php foreach($colors as $color): ?>
 	<a href='#color_<?=str_replace(" ","",$color)?>'>
